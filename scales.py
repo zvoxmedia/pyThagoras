a161cd576aba22be65a3d7e4e7c100d1627d78a9
strHeelJack <donedwardvoiceovers@gmail.com>
2019-08-18 17:42:53 -0400

    Update scales.py

--- scales.py
+++ scales.py
@@ -3,5 +3,5 @@
 # Just trying to create a tuple that represents the chromatic scale
 
 chromScale(range(0,11))
-    for i in range (A, A+/B+, B, C, C+/D-, D, D+/E-, E, F, F+/G-, G, G+/A-)
+    for i in range (A, A+/B-, B, C, C+/D-, D, D+/E-, E, F, F+/G-, G, G+/A-)
 
